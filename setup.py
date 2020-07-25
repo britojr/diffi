34b3c32f0bcd3c43da54e49d52e381bcf9262803
Joao Brito <britojsj@gmail.com>
2020-07-25 13:10:09 -0300

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+# https://packaging.python.org/tutorials/packaging-projects/
+
+setup(
+    name="diffi",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Joao Brito",
+    author_email="",
+    description="",
+)
